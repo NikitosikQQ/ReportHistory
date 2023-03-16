ab46cdcdcfcada851bc3c516c340e87bf89c28d1
NikitosikQQ <120980323+NikitosikQQ@users.noreply.github.com>
2023-03-16 19:24:00 +0300

    Add files via upload

---
+++ ComparatorReport.java
@@ -0,0 +1,22 @@
+package RepHistory;
+
+import java.time.LocalDate;
+
+
+public class ComparatorReport  {
+
+    public static int compare(Object o1, Object o2) {
+        LocalDate date1 = ((Report)o1).getDate();
+        LocalDate date2 = ((Report)o2).getDate();
+        if(date1 == date2){
+            return 0;
+        }
+        if(date1.compareTo(date2) > 0){
+            return -1;
+        }
+        if(date1.compareTo(date2) < 0){
+            return 1;
+        }
+        return 0;
+    }
+}
---
+++ Report.java
@@ -0,0 +1,52 @@
+package RepHistory;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static RepHistory.ReportListToString.reportListToString;
+
+public class Report {
+
+    private Long id;
+
+    private String studentUserName;
+
+    private Integer hours;
+
+    private LocalDate date;
+
+    private String title;
+
+    public String getStudentUserName() {
+        return studentUserName;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Report(Long id, String studentUserName, Integer hours, LocalDate date, String title) {
+        this.id = id;
+        this.studentUserName = studentUserName;
+        this.hours = hours;
+        this.date = date;
+        this.title = title;
+    }
+
+    public static String reportHistory(List<Report> reports, String studentUserName, int count) {
+        List<Report> list = reports.stream().sorted(ComparatorReport::compare).limit(count).collect(Collectors.toList());
+        Collections.reverse(list);
+        return reportListToString(list);
+    }
+}
