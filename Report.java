a9f73e9187ae23ac2427f6bec51c68623f7dcc53
NikitosikQQ <akmn00@mail.ru>
2023-03-21 18:34:29 +0300

    21.03.2023

--- Report.java
+++ Report.java
@@ -1,7 +1,10 @@
 package RepHistory;
 
 import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 
@@ -42,11 +45,12 @@ public class Report {
     }
 
     public static String reportHistory(List<Report> reports, String studentUserName, int count) {
+        Function<Report, LocalDate> sortOfDate = report -> report.getDate();
         return reports.stream()
                 .filter(report -> report.getStudentUserName().equals(studentUserName))
-                .sorted(ComparatorReport::compareDecreaseOfDate)
+                .sorted(Comparator.comparing(sortOfDate).reversed())
                 .limit(count)
-                .sorted(ComparatorReport::compareIncreaseOfDate)
+                .sorted(Comparator.comparing(sortOfDate))
                 .map(report -> report.toString())
                 .collect(Collectors.joining("_____________________" + "\n"));
     }
